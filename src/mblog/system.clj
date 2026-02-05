fabfe8340a0a8ac09cc5747fc139fe38986df01f
Teodor Heggelund <git@teod.eu>
2026-02-05 12:31:58 +0100

    Hold systemet kjørende på clj-reload/reload
    Vi har skrevet all tilstanden vår på en måte som lar oss holde tilstanden mellom
    system-omstart. Nå bruker vi det.
    
    Effekten i praksis? Å relaste systemet eller kjøre testene med clj-reload
    blir *mye* raskere fordi vi ikke bygger databasen på nytt. Nesten umiddelbart,
    faktisk!

--- src/mblog/system.clj
+++ src/mblog/system.clj
@@ -17,64 +17,53 @@
   (db/loaddb {:cohorts db/cohorts :authors db/authors}))
 #_(alter-var-root #'state/datomic create-datomic)
 
-(defn create-file-watcher [db]
-  (fn [previous]
-    (when previous
-      (beholder/stop previous))
-    ;; We watch for changes in local development only, because we don't modify
-    ;; documents in-place in production.
-    (when-not (System/getenv "GARDEN_GIT_REVISION")
-      (let [roots (map :cohort/root (cohort/all db))]
-        (apply beholder/watch
-               (fn [_event]
-                 ;; NOTE: Current reloading behavior is "when ANY doc is
-                 ;; changed, reload EVERY doc". Performance can be improved, but
-                 ;; let's get it correct first.
-                 (let [the-docs (->> (cohort/all db)
-                                     (mapcat db/find-cohort-docs))]
-                   (alter-var-root #'state/datomic
-                                   (fn [olddb]
-                                     (-> olddb
-                                         (d/with the-docs)
-                                         :db-after)))))
-               roots)))))
+(defn create-file-watcher [previous db]
+  (when previous
+    (beholder/stop previous))
+  (when-not (System/getenv "GARDEN_GIT_REVISION")
+    ;; Watch for changes in local development only.
+    (let [roots (map :cohort/root (cohort/all db))]
+      (apply beholder/watch
+             (fn [_event]
+               ;; NOTE: Current reloading behavior is "when ANY doc is changed,
+               ;; reload EVERY doc". So there's possible performance to be
+               ;; gained here.
+               (let [the-docs (->> (cohort/all db)
+                                   (mapcat db/find-cohort-docs))]
+                 (alter-var-root #'state/datomic
+                                 (fn [olddb]
+                                   (-> olddb
+                                       (d/with the-docs)
+                                       :db-after)))))
+             roots))))
 #_(alter-var-root #'state/file-watcher (create-file-watcher state/datomic))
 
-(comment
-  (def db state/datomic)
-  (->> (cohort/all db)
-       (map :cohort/root))
-  (def roots (apply beholder/watch tap> roots))
-  (def olorm (d/entity db [:cohort/id :cohort/olorm]))
-  (def the-docs (db/find-cohort-docs olorm))
-  (-> db (d/with the-docs) :db-after))
-
 (defn create-injected-app [_previous]
-  (let [ring-handler-var (resolve `serve/ring-handler)]
-    (fn [req]
+  (fn [req]
+    (let [ring-handler-var (resolve `serve/ring-handler)]
       (-> req
           (assoc :system/now (Instant/now))
           (assoc :system/datomic state/datomic)
           ring-handler-var))))
 #_(alter-var-root #'state/injected-app create-injected-app)
 
-(defn create-http-server [port]
-  (fn [previous]
-    (when previous
-      (httpkit/server-stop! previous))
-    (httpkit/run-server (fn [req]
-                          (state/injected-app req))
-                        {:port port
-                         :legacy-return-value? false})))
+(defn create-http-server [previous port]
+  (when previous
+    (httpkit/server-stop! previous))
+  (httpkit/run-server (fn [req]
+                        (state/injected-app req))
+                      {:port port
+                       :legacy-return-value? false}))
 #_(alter-var-root #'state/http-server (create-http-server 7223))
-
-;; Obs: kan ikke restarte HTTP-serveren når vi kjører med `garden run`, fordi
-;; HTTP-serveren henger sammen med REPL-serveren. Hvis du prøver å restarte
-;; HTTP-servern i prod, vil du krasje prod (som vil føre til en restart, som er
-;; helt OK, men også kan gjøres med `garden restart`).
 
 (defn dev-start! []
   (require 'dev))
+
+(defn restart [previous start-fn & args]
+  (apply start-fn previous args))
+
+(defn ensure-started [previous start-fn & args]
+  (if previous previous (apply start-fn nil args)))
 
 (defonce !port (atom nil))
 
@@ -83,17 +72,11 @@
   (when (env/dev?) (dev-start!))
   (set! *print-namespace-maps* false)
   (time-literals.read-write/print-time-literals-clj!)
-  (alter-var-root #'state/datomic create-datomic)
-  (alter-var-root #'state/file-watcher (create-file-watcher state/datomic))
-  (alter-var-root #'state/injected-app create-injected-app)
-  (alter-var-root #'state/http-server
-                  ;; (create-http-server (or port 7223))
-                  (fn [old]
-                    (cond
-                      old old
-                      :else ((create-http-server (or port 7223))
-                             nil)))))
+  (alter-var-root #'state/datomic ensure-started create-datomic)
+  (alter-var-root #'state/file-watcher ensure-started create-file-watcher state/datomic)
+  (alter-var-root #'state/injected-app ensure-started create-injected-app)
+  (alter-var-root #'state/http-server ensure-started create-http-server (or port 7223)))
 #_(start! {})
 
-(defn after-ns-reload []
-  (start! {:port @!port}))
+;; (defn after-ns-reload []
+;;   (start! {:port @!port}))
