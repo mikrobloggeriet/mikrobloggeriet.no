a81df6da5aeeece3733bc871b1c1ed0d38f65182
Teodor Heggelund <git@teod.eu>
2026-03-11 21:49:50 +0100

    Remove tangential prose from assetwatch

--- src/terra/assetwatch.clj
+++ src/terra/assetwatch.clj
@@ -15,31 +15,9 @@
   (reset! !watcher nil))
 
 (defn watch!
-  "Watch given folder
-
-  Due to limited attention span, can watch only one folder. If you want to put
-  your assets in many folders, consider first why you want to inflict pain upon
-  yourself. Should you decide, \"yes, for I am an individual whose growth spurs
-  in pain\", [consider inlining](https://play.teod.eu/consider-inlining/) the
-  parts of this namespace which you desire. Then reflect upon whether spreading
-  assets around your disk sparks yoy, or if it's just your pendantics speaking.
-
-  Should you, against the advice of your docstring, attempt to watch a second
-  folder, any previous folder watch attempts will be forgotten. Erased. Washed
-  away by the sands of time.
-
-  Cheers!"
   [folder]
   (when-let [w @!watcher] (stop w))
-  (reset! !folder folder)
-  ;; TODO aaaaachshually also watch the folder in order to do stuff
-  ;;
-  ;; Buuuuuut currently we don't supply any means of *getting* the watched
-  ;; stuff, nor subscribing to events, of any kind. Therefore, of course, we
-  ;; cheat. There is no assetwatch, yet. Only an API shim. Under which the
-  ;; assetwatch may *sneakily* insert itself. In the future, under darkness,
-  ;; while nobody watches.
-  )
+  (reset! !folder folder))
 
 (defn handler "Serve assets from watched folder"
   [{:keys [request-method uri]}]
